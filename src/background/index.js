5367c92fa174251bcf594d72deff7b74c892b202
avikjis27 <avikjis27@gmail.com>
2020-07-27 01:06:04 +0530

    Added logic to parse repo

--- src/background/index.js
+++ src/background/index.js
@@ -14,3 +14,41 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
 	return true;
 });
 
+chrome.commands.onCommand.addListener(function(command) {
+  switch(command){
+    case "follow-repo" :
+      follow_repo();
+			break;
+		case "unfollow-repo" :
+      unfollow_repo();
+    	break;
+  }
+});
+
+function parse_url(url){
+	let parser = document.createElement('a');
+	parser.href = url;
+	let host = parser.host;
+	let path = parser.pathname;
+	let segments = path.split('/').filter(Boolean);
+	let repo = segments.length > 1 ? segments[1] : ""
+	let owner = segments[0]
+	return {
+		domain: host,
+		owner: owner,
+		repo: repo
+	}
+}
+
+function follow_repo(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		console.log(parse_url(tabs[0].url));
+  });
+}
+
+function unfollow_repo(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		console.log(parse_url(tabs[0].url));
+  });
+}
+
